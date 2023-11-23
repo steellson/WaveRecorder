49974fd1d05bcd734d3b120ad4374507db83eac2
Andrey Pocherpaev <@umbrellait-andrey-pochepaevumbrellait>
2023-11-23 14:05:25 +0300

    Initial

---
+++ WaveRecorder/Resources/Resources.swift
@@ -0,0 +1,24 @@
+//
+//  Resources.swift
+//  WaveRecorder
+//
+//  Created by Andrew Steellson on 23.11.2023.
+//
+
+import UIKit
+
+enum R {
+    
+    //MARK: - Strings
+    
+    enum Strings: String {
+        case mainScreenTitle = ""
+    }
+    
+    
+    //MARK: - Colors
+    
+    enum Colors {
+        static let primaryBGColor = UIColor(red: 0/25, green: 0/255, blue: 0/255, alpha: 1)
+    }
+}
--- WaveRecorder/Scene/Main/MainViewController.swift
+++ WaveRecorder/Scene/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift.swift
 //  WaveRecorder
 //
 //  Created by Andrew Steellson on 23.11.2023.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
     }
-
-
 }
 
