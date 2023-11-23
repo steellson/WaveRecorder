0307141f9d0d3d128dd6495db70d80852ab49068
Andrey Pocherpaev <@umbrellait-andrey-pochepaevumbrellait>
2023-11-23 13:50:29 +0300

    Initial Commit

---
+++ WaveRecorder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WaveRecorder
+//
+//  Created by Andrew Steellson on 23.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
