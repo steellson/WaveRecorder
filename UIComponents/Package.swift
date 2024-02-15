17652762d8e902650ce0dc9f99a066ba8a010e6e
Andrew Steellson <113777921+steellson@users.noreply.github.com>
2024-02-15 15:06:07 +0300

    UIComponents restructured

--- UIComponents/Package.swift
+++ UIComponents/Package.swift
@@ -9,7 +9,6 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UIComponents",
             targets: ["UIComponents"]),
@@ -19,12 +18,7 @@ let package = Package(
         .package(path: "../UIKit")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "UIComponents"),
-        .testTarget(
-            name: "UIComponentsTests",
-            dependencies: ["UIComponents"]),
+            name: "UIComponents")
     ]
 )
--- WRParts/Package.swift
+++ WRParts/Package.swift
@@ -9,7 +9,6 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "WRParts",
             targets: ["WRParts"]),
@@ -19,12 +18,7 @@ let package = Package(
         .package(path: "../UIKit")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WRParts"),
-        .testTarget(
-            name: "WRPartsTests",
-            dependencies: ["WRParts"]),
+            name: "WRParts")
     ]
 )
